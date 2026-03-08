32bf387acbc35bb92c916bc63fadfdbd2eb425fb
Amaan Ansari <amaan.huhu@gmail.com>
2026-03-08 20:02:39 +0530

    fix: use lighter model for free tier

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ def get_face_app():
     global face_app
     if face_app is None:
         face_app = insightface.app.FaceAnalysis(
-            name="buffalo_l",
+            name="buffalo_sc",
             providers=["CPUExecutionProvider"],
         )
-        face_app.prepare(ctx_id=0, det_size=(640, 640))
+        face_app.prepare(ctx_id=0, det_size=(320, 320))
     return face_app
 
 
